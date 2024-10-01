34828f01a69ab195efb3f7db2c0edbbf8462ab17
zhaodingyi <zhaodingyi@sjtu.edu.cn>
2024-10-01 21:36:40 +0800

    Homework

---
+++ zhaodingyi/Homework/calibration/build/CMakeFiles/calibration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calibration.dir/calibration.cpp.o"
+  "calibration"
+  "calibration.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
