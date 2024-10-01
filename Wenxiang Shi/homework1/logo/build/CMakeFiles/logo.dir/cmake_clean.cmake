87fe2ceb4ffc9cbcb4492bc5a536161077a9fc27
LuNa-shi <shiwenxiang3525@126.com>
2024-10-01 22:36:31 +0800

    swx added

---
+++ Wenxiang Shi/homework1/logo/build/CMakeFiles/logo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/logo.dir/logo.cpp.o"
+  "CMakeFiles/logo.dir/logo.cpp.o.d"
+  "logo"
+  "logo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/logo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
