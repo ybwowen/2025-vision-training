ddf41ca43057e06e13e05a154ab6267d6aa248a8
Wupei Qin <wupeiqin@sjtu.edu.cn>
2024-10-02 13:29:57 +0800

    Update main.cpp

--- Wu Peiqin/photograph/main.cpp
+++ Wu Peiqin/photograph/main.cpp
@@ -19,7 +19,8 @@
 cv::Mat cameraMatrix, rotationMatrix; // 相机内参矩阵和旋转矩阵
 cv::Mat translationVector; // 平移向量
 
-    std::string filename = "../points.txt";
+std::string filename = "../points.txt";
+
 // 读取坐标
 void readDataFromFile(std::vector<cv::Point3f>& points) {
     std::ifstream file(filename);
@@ -46,13 +47,15 @@ int main() {
     std::vector<cv::Point3f> worldPoints;
     
     // 初始化相机参数
-    cameraMatrix = (cv::Mat_<float>(3,3) << 400., 0., 190., 0.,// 相机内参矩阵
-                                                        0., 400., 160., 0.,
-                                                        0., 0., 1., 0.); 
+    cameraMatrix = (cv::Mat_<float>(3,3) << 400, 0, 190, // 相机内参矩阵
+                                                        0, 400, 160, 
+                                                        0, 0, 1); 
 
-    Eigen::Quaterniond quaterniond(0.5,0.5,-0.5,-0.5);// 通过四元数转换得到的旋转矩阵
+    Eigen::Quaterniond quaterniond(-0.5,0.5,0.5,-0.5);// 通过四元数转换得到的旋转矩阵
     Eigen::Matrix3f rot_Matrix = quaterniond.toRotationMatrix().cast<float>(); 
     rotationMatrix=cv::Mat(rot_Matrix.rows(),rot_Matrix.cols(),CV_32F,rot_Matrix.data());
+    cv::Mat invert_rot;
+    cv::invert(rotationMatrix,invert_rot);
 
     translationVector = cv::Mat(3,1,CV_32F,cv::Scalar(2,2,2)); // 平移向量
 
@@ -61,22 +64,27 @@ int main() {
     readDataFromFile( worldPoints);
 
     // 创建图像并设置大小（根据需要调整）
-    cv::Mat image(720, 720, CV_8UC3, cv::Scalar(255,255, 255));
+    cv::Mat image(1080, 1080, CV_8UC3, cv::Scalar(255,255, 255));
 
     // 投影并绘制点
 
     for (const cv::Point3f& point3D : worldPoints) 
     {
+        std::cout<<"-----------------------------------------"<<std::endl;
         // 将3D点转换为相机坐标系下的点
         cv::Mat point3D_cv = (cv::Mat_<float>(3, 1) << point3D.x, point3D.y, point3D.z);
-        cv::Mat pointCam=rotationMatrix*point3D_cv+translationVector;
+        std::cout<<point3D_cv<<std::endl;
+        cv::Mat pointCam=invert_rot*point3D_cv-translationVector;
+        std::cout<<pointCam<<std::endl;
 
         // 投影到图像平面
         cv::Mat point2D=cameraMatrix*pointCam;
+        std::cout<<point2D<<std::endl;
         point2D=point2D/point2D.at<float>(2,0);
 
         // 绘制点
-        cv::circle(image, cv::Point(cvRound(point2D.at<double>(0, 0)), cvRound(point2D.at<double>(1, 0))), 2, cv::Scalar(0, 0, 255), -1);
+        std::cout<<point2D<<std::endl;
+        cv::circle(image, cv::Point(cvRound(point2D.at<float>(0, 0)), cvRound(point2D.at<float>(1, 0))), 2, cv::Scalar(0, 0, 255), -1);
     }
 
     // 显示图像
