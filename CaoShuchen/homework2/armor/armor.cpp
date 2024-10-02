4a108f8777bb69437d3dce8981eaa0bbfc2f6f79
cwlm_ma <cwlm_ma@sjtu.edu.cn>
2024-10-02 17:09:46 +0800

    feat: read

--- CaoShuchen/homework2/armor/armor.cpp
+++ CaoShuchen/homework2/armor/armor.cpp
@@ -2,7 +2,80 @@
 // Created by cwlm on 2024/10/2.
 //
 
-int main(int argc, char ** argv)
-{
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <opencv2/opencv.hpp>
+#include <Eigen/Dense>
+
+const std::vector<cv::Point3d> PW_BIG{
+    // 灯条坐标，单位：m
+    {-0.115, 0.0265, 0.},
+    {-0.115, -0.0265, 0.},
+    {0.115, -0.0265, 0.},
+    {0.115, 0.0265, 0.}
+};
+
+Eigen::Quaterniond parseQuaternion(const std::string& str) {
+    std::vector<double> numbers;
+    std::string temp = str;
+    size_t pos = 0;
+
+    while ((pos = temp.find_first_of('+')) != std::string::npos) {
+        std::string token = temp.substr(0, pos);
+        temp.erase(0, pos + 1);
+        numbers.push_back(std::stod(token));
+    }
+    numbers.push_back(std::stod(temp));
+
+    return {numbers[1], numbers[2], numbers[3], numbers[0]};
+}
+
+
+void readIMUAndArmor(const std::string& filename, Eigen::Quaterniond& imuQuat, std::vector<cv::Point2f>& armorPoints) {
+    std::ifstream infile(filename);
+    std::string line;
+
+    std::getline(infile, line);
+    std::getline(infile, line); // 读取四元数
+    imuQuat = parseQuaternion(line);
+    std::getline(infile, line);
+    for (int i = 0; i < 4; ++i) {
+        std::getline(infile, line);
+        std::stringstream ss(line);
+        cv::Point2f point;
+        ss >> point.x >> point.y;
+        armorPoints.push_back(point);
+    }
+
+    infile.close();
+}
+
+void readCameraMatrixAndDistCoeffs(const std::string& filename, cv::Mat& cameraMatrix, cv::Mat& distCoeffs) {
+    cv::FileStorage fs(filename, cv::FileStorage::READ);
+    fs["F"] >> cameraMatrix;
+    fs["C"] >> distCoeffs;
+    fs.release();
+}
+
+int main() {
+    // 读取内参和畸变参数
+    cv::Mat cameraMatrix, distCoeffs;
+    readCameraMatrixAndDistCoeffs("assets/f_mat_and_c_mat.yml", cameraMatrix, distCoeffs);
+
+    // 读取 IMU 和装甲板坐标
+    Eigen::Quaterniond imuQuat;
+    std::vector<cv::Point2f> armorPoints;
+    readIMUAndArmor("assets/imu_and_armor.txt", imuQuat, armorPoints);
+
+    std::cout << "Camera Matrix:\n" << cameraMatrix << std::endl;
+    std::cout << "Distortion Coefficients:\n" << distCoeffs << std::endl;
+    std::cout << "IMU Quaternion: " << imuQuat << std::endl;
+    std::cout << "Armor Points:\n";
+    for (const auto& point : armorPoints) {
+        std::cout << point << std::endl;
+    }
+
     return 0;
 }
