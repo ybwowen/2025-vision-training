9db8c9ee6de95b28dae2ed9bc4fc7bbc3c6a13f9
kodatemitsuru <hh178239456@gmail.com>
2024-10-03 13:24:55 +0800

    homework2: logo: pretty output

--- 胡哲铖/homework2/logo/logo.cpp
+++ 胡哲铖/homework2/logo/logo.cpp
@@ -1,4 +1,5 @@
 #include <eigen3/Eigen/Dense>
+#include <opencv2/calib3d.hpp>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -36,63 +37,60 @@ int main() {
     vector<vector<Point>> contours;
     findContours(th, contours, cv::RETR_TREE, CHAIN_APPROX_SIMPLE);
     // 将轮廓点转换为三维
+
+    // 相机内参矩阵
+    Mat cameraMatrix = (Mat_<double>(3, 3) <<   400, 0., 0.,
+                                                0., 400, 0.,
+                                                0., 0., 1.);
+   // Mat invmat = cameraMatrix.
     int numPoints{0};
     vector<Point3d> points;
     for (size_t i = 0; i < contours.size(); i++) {
         for (const auto& point : contours[i]) {
-            points.push_back(Point3d(point.x ,point.y,generateRandomDepth(0, 500.0)));
+            double zc = generateRandomDepth(0.0, 100000.0);
+            points.push_back(Point3d(point.x*zc/cameraMatrix.at<double>(0, 0) ,point.y*zc/cameraMatrix.at<double>(1, 1),zc));
             numPoints++;
         }
     }
-    
-
-
-    // 相机内参矩阵
-    Mat cameraMatrix = (Mat_<double>(3, 3) <<   width, 0., 0,
-                                                0., height, 0,
-                                                0., 0., 1.);
-
+    for (size_t j = 0; j < 100; j++){ //气氛组
+        points.push_back(Point3d(generateRandomDepth(0.0, width) ,generateRandomDepth(0.0, height),generateRandomDepth(0.0, 100000.0)));
+    }
     vector<Mat>images;
     //画动画帧
     for (int i{0};i < 122 ;i++){
-        Matrix3d rotateMatrixX,rotateMatrixY,rotateMatrix;
-        double theta = i/31.4;
-        //偷个懒
-        rotateMatrixX << 1, 0, 0,
-                        0, cos(8/31.0), sin(8/31.0),
-                        0, -sin(8/31.0), cos(8/31.0);
-        rotateMatrixY << cos(i/31.0), 0, sin(i/31.0),
+        Matrix3d rotateMatrix;
+        double theta = pow(1.012,i)-1;
+        Quaterniond rotateQuat(cos(theta/2),0.424*sin(theta/2),0.707*sin(theta/2),0.566*sin(theta/2));
+        rotateMatrix << cos(theta), 0, sin(theta),
                         0, 1, 0,
-                        -sin(i/31.0), 0, cos(i/31.0);
-        //合成旋转矩阵
-        rotateMatrix = rotateMatrixX*rotateMatrixY;
-
-        Mat black = Mat::zeros(width*2, height*2, CV_8UC3);
+                        -sin(theta), 0, cos(theta);
+        
+        Mat black = Mat::zeros(width, height, CV_8UC3);
         for (const auto& point : points){
-            Vector3d cameraPos(-2000,0,-1000); 
             Vector3d pointPosInWorld(point.x,point.y,point.z);
-            Vector3d pointPosInCamera = rotateMatrix* pointPosInWorld - cameraPos; //很奇怪，相机初始并不是对着图像正前方的
+            Vector3d pointPosInCamera = rotateQuat* pointPosInWorld; 
             double x = cameraMatrix.at<double>(0, 0) * pointPosInCamera[0] + cameraMatrix.at<double>(0, 2) * pointPosInCamera[2];
             double y = cameraMatrix.at<double>(1, 1) * pointPosInCamera[1] + cameraMatrix.at<double>(1, 2) * pointPosInCamera[2];
-            double z = pointPosInCamera[2];
+            double z = pointPosInCamera[2] + i*10;
             Point2f pointPosInImg(x / z, y / z);
-            circle(black, pointPosInImg, 0, Scalar(255, 255, 255), 20-z/70);
+            circle(black, pointPosInImg, 0, Scalar(255, 255, 255), 10-z/7000);
         }
-        flip(black,black, 1);
         images.push_back(black);
-        if (i==121){
-            for (int j{0};j < 119 ;j++){
+        
+        if (i==0){
+            for (int k{0};k < 119 ;k++){ //停一下
                 images.push_back(black);
             }
         }
+        
     }
     VideoWriter writer;
-    writer.open("../logo.avi", VideoWriter::fourcc('M', 'J', 'P', 'G'), 60, size*2, true);
+    writer.open("../logo.avi", VideoWriter::fourcc('M', 'J', 'P', 'G'), 60, size, true);
     
     int t{0};
     
-
-    for (auto& frame : images){
+    reverse(images.begin(),images.end());
+    for (Mat frame : images){
         writer.write(frame);
     }
     writer.release();
