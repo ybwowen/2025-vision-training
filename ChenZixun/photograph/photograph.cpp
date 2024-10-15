f046cc9b6c9c069e9aa942944041ff25dacce85a
Parahell43 <Gipellxun@outlook.com>
2024-10-15 23:34:08 +0800

    correct photograph

--- ChenZixun/photograph/photograph.cpp
+++ ChenZixun/photograph/photograph.cpp
@@ -1,10 +1,13 @@
+// #include <eigen3/Eigen/src/Core/Matrix.h>
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <opencv2/opencv.hpp>
+#include <Eigen/Dense>
 
 using namespace std;
 using namespace cv;
+using namespace Eigen;
 
 // p_w -> p_c
 // p_c = R * (p_w - T)
@@ -22,19 +25,28 @@ int main() {
   ifstream Input("../points.txt");
   Input >> n;
   // cout << n << endl;
-  Mat Image(1024, 2000, CV_8UC3, Scalar(0, 0, 0));
-  Mat Rot = (Mat_<double>(3, 3) << 0, 0, -1, 1, 0, 0, 0, -1, 0), T = (Mat_<double>(3, 1) << 2, 2, 2); // Rot 旋转矩阵  T 平移向量
-  Mat K = (Mat_<double>(3, 3) << 400, 0, 190, 0, 400, 160, 0, 0, 1); // 相机内参矩阵
+  Mat Image(1024, 1043, CV_8UC3, Scalar(0, 0, 0));
+  Quaterniond q(-0.5, 0.5, 0.5, -0.5);
+  Matrix3d Rot = q.toRotationMatrix();
+  Matrix3d R = Rot.transpose(); // R 旋转矩阵
+  Vector3d T(2, 2, 2); // T 平移向量
+  Matrix3d K;
+  K << 400, 0, 190,
+       0, 400, 160,
+       0, 0, 1; // 相机内参矩阵
+
   for (int i = 0; i < n; ++i) {
-    double x, y, z;
-    Input >> x >> y >> z;
-    Mat P = (Mat_<double>(3, 1) << x, y, z);
-    P = Rot * (P - T);
+    double tx, ty, tz;
+    Input >> tx >> ty >> tz;
+    Vector3d P(tx, ty, tz);
+    P = R * (P - T);
     P = K * P;
-    double u = P.at<double>(0, 0), v = P.at<double>(1, 0);
+    P /= P(2);
+    double u = P(0), v = P(1);
     // cout << u << " " << v << endl;
     circle(Image, Point(u, v), 1, Scalar(255, 255, 255), 1);
   }
+
   imshow("Image", Image);
   waitKey(0);
   return 0;
