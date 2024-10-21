67aba462ff00258bd76246d06ab98613139761f9
Parahell43 <Gipellxun@outlook.com>
2024-10-21 22:33:06 +0800

    modify logo

--- ChenZixun/logo/logo.cpp
+++ ChenZixun/logo/logo.cpp
@@ -1,9 +1,9 @@
-// 不知为何生成视频文件一直无法打开，而且每一帧的图像都没有问题，过两天再来调。一直看不到最终效果，我想的是有顺序地改变四元数来调整相机位姿，不太确定对不对，没办法看到最终效果
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <opencv2/opencv.hpp>
 #include <Eigen/Dense>
+#include <Eigen/Geometry>
 
 using namespace std;
 using namespace cv;
@@ -30,11 +30,12 @@ int main() {
       0, 400, 160,
       0, 0, 1; // 相机内参矩阵
 
-  VideoWriter Logo("../LogoGif.avi", VideoWriter::fourcc('M', 'J', 'P', 'G'), 30, Size(1024, 1040));
+  VideoWriter Vout("../Logo.avi", VideoWriter::fourcc('X', 'V', 'I', 'D'), 30, Size(1024, 1040));
 
-  for (int i = 10; i >= 0; --i) {
-    double t = (double)i;
-    Quaterniond q(-0.5 - t, 0.5 - t, 0.5 - t, -0.5 - t);
+  Quaterniond q0(1, 0, 0, 0), q1(-0.5, 0.5, 0.5, -0.5);
+
+  for (double t = 0; t <= 1; t += 0.1) {
+    Quaterniond q = q0.slerp(t, q1);
     Matrix3d Rot = q.toRotationMatrix();
     Matrix3d R = Rot.transpose(); // R 旋转矩阵
 
@@ -45,20 +46,18 @@ int main() {
       P = K * P;
       P /= P(2);
       double u = P(0), v = P(1);
-      circle(Image, Point(u, v), 1, Scalar(255, 0, 0), 1);
+      circle(Image, Point(u, v), 1, Scalar(255, 255, 255), 1);
     }
     // Mat ImageColor;
     // cvtColor(Image, ImageColor, COLOR_GRAY2BGR);
     // Images.push_back(ImageColor);
-    Logo.write(Image);
-    // if (qi == -1.5 && qj == 0.5 && qk == 0.5 && qw == -0.5) {
-      // imshow("Image", Image);
-      // waitKey(0);
-    // }
+    Vout.write(Image);
+    // imshow("Image", Image);
+    // waitKey(0);
   }
 
-  Logo.release();
-  destroyAllWindows();
+  Vout.release();
+  // destroyAllWindows();
 
   return 0;
 }
