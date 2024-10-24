4fd140ba3bf724bcd4ae064cff351760bfa6c429
JimmyJr-Official <98706140+JimmyJr-Official@users.noreply.github.com>
2024-10-24 10:51:07 +0800

    Add files via upload

--- Zhang Jiajie/homework1/Logo/main.cpp
+++ Zhang Jiajie/homework1/Logo/main.cpp
@@ -11,6 +11,7 @@ using namespace Eigen;
 using namespace cv;
 
 int main(){
+    double rad = 750.0;
     Mat img = imread("../img.png");
     Mat img_gray;
     cvtColor(img, img_gray, COLOR_BGR2GRAY);
@@ -25,8 +26,10 @@ int main(){
     vector<int> rad_list;
         for(int i=0; i<contours.size(); i++){
             for(int j=0; j<contours[i].size(); j++){
-                double z = rand()%200-100.0;
-                pos_list.push_back(Vector4d(contours[i][j].x, contours[i][j].y, z, 1.0));
+                double z = rand()%500-250.0;
+                double scale = rad/(rad+z);
+                pos_list.push_back(Vector4d((contours[i][j].x-472.0)*scale,
+                                             (contours[i][j].y-267.0)*scale, z, 1.0));
                 rad_list.push_back(rand()%4+1);
             }
         }
@@ -38,10 +41,11 @@ int main(){
             0., 400., 160., 0.,
             0., 0., 1., 0.;
 
+    Mat image = Mat(500, 700, CV_8UC3, Scalar(0,0,0));
     for(int t=0; t<100; t++){
         double angle = t*M_PI/200.0;
 
-        Vector3d T(750.0*cos(angle)+472.0, 267.0, 750.0*sin(angle));;
+        Vector3d T(rad*cos(angle), 0, rad*sin(angle));;
         Matrix3d Mr = AngleAxisd(M_PI/2-angle, Vector3d(0, 1, 0)).toRotationMatrix();
         
         Matrix<double, 4, 4> M0;
@@ -50,7 +54,7 @@ int main(){
                 Mr(2, 0), Mr(2, 1), Mr(2, 2), T(2),
                 0, 0, 0, 1;
 
-        Mat image = Mat(500, 700, CV_8UC3, Scalar(0,0,0));
+        image = Mat(500, 700, CV_8UC3, Scalar(0,0,0));
         for(int i=0; i<pos_list.size(); i++){
             Vector<double, 3> posr = Mc * M0.inverse() * pos_list[i];
             Point p(-posr(0)/posr(2)+500, -posr(1)/posr(2)+400);
@@ -60,7 +64,11 @@ int main(){
         imshow("Photo", image);
         waitKey(25);
     }
+    for(int i=0; i<20; i++){
+        video.write(image);
+        imshow("Photo", image);
+        waitKey(25);
+    }
     video.release();
     return 0;
 }
-
