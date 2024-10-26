2e02aeb23a32fe4f1f2ef4f342ceadd7c10afbf2
Parahell43 <Gipellxun@outlook.com>
2024-10-26 11:56:10 +0800

    homework4

--- ChenZixun/homework4/1div/src/division/src/division_client.cpp
+++ ChenZixun/homework4/1div/src/division/src/division_client.cpp
@@ -1,5 +1,5 @@
-#include "rclcpp/rclcpp.hpp"
 #include "division/srv/DivideInt.hpp"
+#include "rclcpp/rclcpp.hpp"
 #include <chrono>
 
 using DivideInt = division::srv::Divideint;
@@ -7,48 +7,36 @@ using namespace std::chrono_literals;
 
 class DivisionClient : public rclcpp::Node {
 public:
-    DivisionClient() : Node("division_client")
-    {
-        client_ = this->create_client<DivideInt>("divide_two_ints");
+  DivisionClient() : Node("division_client") {
+    client_ = this->create_client<DivideInt>("divide_two_ints");
+  }
+
+  void send_request(int64_t a, int64_t b) {
+    auto request = std::make_shared<DivideInt::Request>();
+    request->a = a;
+    request->b = b;
+
+    auto result = client_->async_send_request(request);
+    if (rclcpp::spin_until_future_complete(this->get_node_base_interface(),
+                                           result) ==
+        rclcpp::FutureReturnCode::SUCCESS) {
+      auto response = result.get();
+      RCLCPP_INFO(this->get_logger(), "Response: q=%ld, r=%ld", response->q,
+                  response->r);
+    } else {
+      RCLCPP_ERROR(this->get_logger(), "Fail");
     }
-
-    void send_request(int64_t a, int64_t b)
-    {
-        auto request = std::make_shared<DivideInt::Request>();
-        request->a = a;
-        request->b = b;
-
-        while (!client_->wait_for_service(1s))
-        {
-            RCLCPP_WARN(this->get_logger(), "Waiting for the service to be available...");
-        }
-
-        auto result = client_->async_send_request(request);
-        if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) == rclcpp::FutureReturnCode::SUCCESS)
-        {
-            auto response = result.get();
-            RCLCPP_INFO(this->get_logger(), "Response: q=%ld, r=%ld", response->q, response->r);
-        }
-        else
-        {
-            RCLCPP_ERROR(this->get_logger(), "Failed to call service divide_two_ints");
-        }
-    }
+  }
 };
 
 int main(int argc, char **argv) {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<DivisionClient>();
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<DivisionClient>();
 
-    if (argc != 3) {
-        RCLCPP_ERROR(node->get_logger(), "Usage: division_client a b");
-        return 1;
-    }
+  int64_t a = std::stoll(argv[1]);
+  int64_t b = std::stoll(argv[2]);
 
-    int64_t a = std::stoll(argv[1]);
-    int64_t b = std::stoll(argv[2]);
-
-    node->send_request(a, b);
-    rclcpp::shutdown();
-    return 0;
+  node->send_request(a, b);
+  rclcpp::shutdown();
+  return 0;
 }
--- ChenZixun/homework4/1div/src/division/src/division_server.cpp
+++ ChenZixun/homework4/1div/src/division/src/division_server.cpp
@@ -1,45 +1,41 @@
-#include "rclcpp/rclcpp.hpp"
 #include "division/srv/DivideInt.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 using DivideInt = division::srv::Divideint;
 
 class DivisionServer : public rclcpp::Node {
 public:
-    DivisionServer() : Node("division_server")
-    {
-        service_ = this->create_service<DivideInt>(
-            "divide_two_ints", 
-            std::bind(&DivisionServer::handle_service, this, std::placeholders::_1, std::placeholders::_2)
-        );
-        RCLCPP_INFO(this->get_logger(), "Service 'divide_two_ints' ready to receive requests.");
-    }
+  DivisionServer() : Node("division_server") {
+    service_ = this->create_service<DivideInt>(
+        "divide_two_ints",
+        std::bind(&DivisionServer::handle_service, this, std::placeholders::_1,
+                  std::placeholders::_2));
+    RCLCPP_INFO(this->get_logger(),
+                "Service 'divide_two_ints' ready to receive requests.");
+  }
 
 private:
-    void handle_service(const std::shared_ptr<DivideInt::Request> request,
-                        std::shared_ptr<DivideInt::Response> response)
-    {
-        if (request->b == 0)
-        {
-            RCLCPP_WARN(this->get_logger(), "Division by zero is undefined.");
-            response->q = 0;
-            response->r = 0;
-        }
-        else
-        {
-            response->q = request->a / request->b;
-            response->r = request->a % request->b;
-            RCLCPP_INFO(this->get_logger(), "Request: a=%ld, b=%ld => q=%ld, r=%ld",
-                        request->a, request->b, response->q, response->r);
-        }
+  void handle_service(const std::shared_ptr<DivideInt::Request> request,
+                      std::shared_ptr<DivideInt::Response> response) {
+    if (request->b == 0) {
+      RCLCPP_WARN(this->get_logger(), "Division by zero is undefined.");
+      response->q = 0;
+      response->r = 0;
+    } else {
+      response->q = request->a / request->b;
+      response->r = request->a % request->b;
+      RCLCPP_INFO(this->get_logger(), "Request: a=%ld, b=%ld => q=%ld, r=%ld",
+                  request->a, request->b, response->q, response->r);
     }
+  }
 
-    rclcpp::Service<DivideInt>::SharedPtr service_;
+  rclcpp::Service<DivideInt>::SharedPtr service_;
 };
 
 int main(int argc, char **argv) {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<DivisionServer>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<DivisionServer>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
 }
